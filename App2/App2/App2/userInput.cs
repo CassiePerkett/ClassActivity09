b47a6cf9c4a50e02e00f1b1ec3bdb29e2353ea7f
CassiePerkett <u16143923@tuks.co.za>
2019-04-25 13:56:47 +0200

    Initial

---
+++ App2/App2/App2/userInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.AppCompat;
+using App2.Resources.Model;
+using App2.Resources.Helper;
+
+namespace App2
+{
+    [Activity(Label = "userInput")]
+    public class UserInput : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            
+
+
+        }
+    }
+}
